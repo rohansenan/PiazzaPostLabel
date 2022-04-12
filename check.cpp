d1486194a44e3c9e868b1e74fa24a3c7daa3213c
rsenan <rsenan@umich.edu>
2022-04-12 18:41:04 -0400

    bit of main

--- check.cpp
+++ check.cpp
@@ -2,22 +2,47 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <set> 
 using namespace std;
 
+set<string> unique_words(const string &str) {
+    istringstream source(str);
+    set<string> words;
+    string word;
+    while (source >> word) {
+    words.insert(word);
+    }
+    return words;
+}
+
+void add_words_to_map(string &words, string label, map<string, map<string, int> > &words_per_label)
+{
+    set<string> wordset = unique_words(words);
+    for (auto &word:wordset)
+    {
+        words_per_label[label][word] += 1;
+    }
+}
 
 int main() {
-  // Open file
   csvstream csvin("train_small.csv");
     map<string, string> row;
-    map<string, string> tag_with_content;
+    map<string, int> labels;
+    map<string, map<string, int> > words_per_label;
     while(csvin >> row)
     {
         for (auto &col:row)
         {
+            string label;
             if (col.first == "tag")
             {
-                row[col.second]
-                
+                labels[col.second] += 1;
+                words_per_label[col.second];
+                label = col.second;
+            }
+            else if (col.first == "content")
+            {
+                add_words_to_map(col.second, label, words_per_label);
             }
         }
     }
--- main.cpp
+++ main.cpp
@@ -3,10 +3,57 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <set>
 #include <string>
 #include "csvstream.h"
 
 using namespace std;
+
+set<string> unique_words(const string &str) {
+    istringstream source(str);
+    set<string> words;
+    string word;
+    while (source >> word) {
+    words.insert(word);
+    }
+    return words;
+}
+
+string add_words_to_map(string &words, string label, map<string, map<string, int> > &words_per_label)
+{
+    set<string> wordset = unique_words(words);
+    for (auto &word:wordset)
+    {
+        words_per_label[label][word] += 1;
+    }
+    return words;
+}
+
+string create_maps(map<string, string> &row, map<string, int> &labels, map<string, map<string, int> > &words_per_label)
+{
+    string word;
+    for (auto &col:row)
+    {
+        string label;
+        if (col.first == "tag")
+        {
+            labels[col.second] += 1;
+            words_per_label[col.second];
+            label = col.second;
+        }
+        else if (col.first == "content")
+        {
+            word = add_words_to_map(col.second, label, words_per_label);
+        }
+    }
+    return word;
+}
+
+string word_compile(string &words_prev, string &words_new)
+{
+    return words_prev + words_new;
+}
+
 
 class Classifier
 {
@@ -17,17 +64,36 @@ class Classifier
         map<string, map<string, double> > words_per_label;
     public:
         Classifier(string inputFile)
+        : number_of_posts(0), vocabulary_size(0)
         {
-            csvstream csvin(inputFile);
+            csvstream csvin("train_small.csv");
             map<string, string> row;
+            map<string, int> labels;
+            map<string, map<string, int> > words_per_label;
+            string words_prev = "";
             while(csvin >> row)
             {
+                number_of_posts++;
+                string words_new = create_maps(row, labels, words_per_label);
+                words_prev += " " + words_new;
             }
-            
+            set<string> wordset = unique_words(words_prev);
+            vocabulary_size = wordset.size();
+        }
+
+        int get_number_of_posts()
+        {
+            return number_of_posts;
+        }
+
+        int get_vocab_size()
+        {
+            return vocabulary_size;
         }
 };
 
 int main()
 {
-    cout << "hello" << endl;
+    Classifier classifier("train_small.csv");
+    cout << classifier.get_number_of_posts() << " " << classifier.get_vocab_size() << endl;
 }
